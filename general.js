41a20e852e3eec42b7bccdae56d1904456d76d7a
KevinAdiel <201020724@ingenieria.usac.edu.gt>
2016-12-04 23:10:28 -0600

    Manejo de sesión
    Se agregó el manejo de sesión en la página de perfil asi como se creo la
    página de perfil a falta de conexión con datos reales.

---
+++ general.js
@@ -0,0 +1,28 @@
+function cargarVariables() {
+	cadVariables = location.search.substring(1, location.search.length);
+	arrVariables = cadVariables.split("&");
+	for (i = 0; i < arrVariables.length; i++) {
+		arrVariableActual = arrVariables[i].split("=");
+		if (i == 0) {
+			token = arrVariableActual[1];
+		} else {
+			usuario = arrVariableActual[1];
+		}
+	}
+}
+
+function enviarInfo(pagina) {
+	pagina = pagina + ".html";
+	nombres = "token,usuario";
+	pagina += "?";
+	nomVec = nombres.split(",");
+	for (i = 0; i < nomVec.length; i++) {
+		if (i == 0) {
+			pagina += nomVec[i] + "=" + token + "&";
+		} else {
+			pagina += nomVec[i] + "=" + usuario + "&";
+		}
+	}
+	pagina = pagina.substring(0, pagina.length - 1);
+	location.href = pagina;
+}
